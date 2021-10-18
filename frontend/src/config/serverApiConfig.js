80bc22795451a8eda9a313a8edee98f68f807320
Salah Eddine Lalami <lalami.sdn@gmail.com>
2021-10-18 17:00:06 +0100

    update config file

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://antd-admin-mern.idurar.net/api/";
+export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
 // export const API_BASE_URL = "http://localhost:8888/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";

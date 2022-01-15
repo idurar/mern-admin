75aa3dfa77fd2f0ad1fb7534877bb42472ee4ce2
idurar <lalami.sdn@gmail.com>
2022-01-15 13:29:57 +0100

    update server config

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,3 +1,6 @@
-export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
-// export const API_BASE_URL = "http://localhost:8888/api/";
-export const ACCESS_TOKEN_NAME = "x-auth-token";
+export const API_BASE_URL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:8888/api/"
+    : "https://starter-mern.herokuapp.com/api/"
+// export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
+export const ACCESS_TOKEN_NAME = "x-auth-token"
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 // import "./index.css";
-import App from "./app";
-import "./style/app.less";
-import * as serviceWorker from "./serviceWorker";
+import App from "./app"
+import "./style/app.less"
+import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+console.log("process env", process.env.NODE_ENV)
+
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()

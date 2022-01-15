941398b0a7dc07700f1dcf06b02dfa42433ff7f3
idurar <lalami.sdn@gmail.com>
2022-01-15 15:35:28 +0100

    add remote dev

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,6 +1,13 @@
+console.log(
+  "process env REACT_APP_DEV_REMOTE",
+  process.env.REACT_APP_DEV_REMOTE
+)
+
 export const API_BASE_URL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:8888/api/"
-    : "https://starter-mern.herokuapp.com/api/"
+  process.env.NODE_ENV == "production" ||
+  process.env.REACT_APP_DEV_REMOTE == "remote"
+    ? "https://starter-mern.herokuapp.com/api/"
+    : "http://localhost:8888/api/"
+
 // export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token"

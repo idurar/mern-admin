7cecfb2e7e39913aa012f9f9f5c938cff66a1332
idurar <lalami.sdn@gmail.com>
2023-03-22 11:32:27 +0100

    update remote server url

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,8 +6,8 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://starter-mern.herokuapp.com/api/"
+    ? "https://wild-puce-reindeer-sari.cyclic.app/api/"
     : "http://localhost:8888/api/"
 
-// export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
+// export const API_BASE_URL = "https://wild-puce-reindeer-sari.cyclic.app/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token"

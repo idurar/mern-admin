3dcd6832df6919f984436b223c483654135444f0
Salah Eddine Lalami <136928179+salahlalami@users.noreply.github.com>
2023-09-22 01:50:37 +0100

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,7 +6,7 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://wild-puce-reindeer-sari.cyclic.app/api/"
+    ? "https://erp-crm.idurarapp.com/api/"
     : "http://localhost:8888/api/"
 
 // export const API_BASE_URL = "https://wild-puce-reindeer-sari.cyclic.app/api/";
